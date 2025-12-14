19d0550df5d4bdcbdc458c2f856dd76cb69a37b8
jDamodharan <jdamodharan.cs2023@citchennai.net>
2025-12-14 14:09:04 +0530

    fix1

--- api/config.js
+++ api/config.js
@@ -1,8 +1,4 @@
 export default function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method Not Allowed" });
-  }
-
   res.status(200).json({
     serviceId: process.env.SERVICE_ID,
     templateOwner: process.env.TEMPLATE_OWNER,
