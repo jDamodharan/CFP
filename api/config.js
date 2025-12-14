49178ce4a15cfea4fd16e035134c52d7c3724256
jDamodharan <jdamodharan.cs2023@citchennai.net>
2025-12-14 13:37:01 +0530

    cfp

---
+++ api/config.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  // Return EmailJS configuration from env vars
+  res.status(200).json({
+    serviceId: process.env.SERVICE_ID,
+    templateOwner: process.env.TEMPLATE_OWNER,
+    templateAutoReply: process.env.TEMPLATE_AUTOREPLY,
+    publicKey: process.env.PUBLIC_KEY
+  });
+}
