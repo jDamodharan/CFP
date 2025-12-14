0ecfc5f33b0c718f8bd5666391b0584d1ab6926c
jDamodharan <jdamodharan.cs2023@citchennai.net>
2025-12-14 20:08:02 +0530

    otp&verify

--- api/config.js
+++ api/config.js
@@ -1,8 +1,15 @@
 export default function handler(req, res) {
   res.status(200).json({
-    serviceId: process.env.SERVICE_ID,
-    templateOwner: process.env.TEMPLATE_OWNER,
-    templateAutoReply: process.env.TEMPLATE_AUTOREPLY,
-    publicKey: process.env.PUBLIC_KEY
+    main: {
+      serviceId: process.env.SERVICE_ID,
+      templateOwner: process.env.TEMPLATE_OWNER,
+      templateAutoReply: process.env.TEMPLATE_AUTOREPLY,
+      publicKey: process.env.PUBLIC_KEY
+    },
+    otp: {
+      serviceId: process.env.OTP_SERVICE_ID,
+      templateOtp: process.env.OTP_TEMPLATE_ID,
+      publicKey: process.env.OTP_PUBLIC_KEY
+    }
   });
 }
