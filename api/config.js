70682f83b049d93f54cd38407092436c3ea8cad1
jDamodharan <jdamodharan.cs2023@citchennai.net>
2025-12-14 13:52:17 +0530

    cfp-fixed2

--- api/config.js
+++ api/config.js
@@ -1,5 +1,8 @@
 export default function handler(req, res) {
-  // Return EmailJS configuration from env vars
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+
   res.status(200).json({
     serviceId: process.env.SERVICE_ID,
     templateOwner: process.env.TEMPLATE_OWNER,
